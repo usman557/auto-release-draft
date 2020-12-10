815ebf3be1cb829e4e08384c1108b7f0054bedb2
Muhammad Usman <muhammad.usman@contour-software.com>
2020-12-10 19:33:02 +0500

    removed the error.

--- src/wait.ts
+++ src/wait.ts
@@ -1,7 +1,7 @@
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
+      console.log('milliseconds not a number')
     }
 
     setTimeout(() => resolve('done!'), milliseconds)
